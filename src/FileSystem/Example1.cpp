eeabc8453ed9a9fe4f96c886eda8be416347b44d
lgomez <lgomez@hexacta.com>
2017-05-05 10:32:11 -0300

    Initial solution

---
+++ src/FileSystem/Example1.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Example1.h"
+
+
+Example1::Example1()
+{
+}
+
+
+Example1::~Example1()
+{
+}
---
+++ src/FileSystem/Example1.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <boost/filesystem/path.hpp>
+
+class Example1
+{
+public:
+	Example1();
+	~Example1();
+};
+
